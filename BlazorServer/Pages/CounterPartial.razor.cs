68374d078d6da826d275a518d5b103d949dec7bb
Luan Freitas <luan.lrf@gmail.com>
2020-09-24 15:32:33 -0300

    Create partial and base class

---
+++ BlazorServer/Pages/CounterPartial.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public partial class CounterPartial
+    {
+        private int currentCountPartial = 0;
+
+        private void IncrementCountPartial()
+        {
+            currentCountPartial++;
+        }
+    }
+}
